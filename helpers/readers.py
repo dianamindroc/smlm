66f3ba1478ef444dc2747398a8470321cf720d48
Diana <dianamindrocf@gmail.com>
2022-02-04 17:28:12 +0100

    add visualization, update readers

--- helpers/readers.py
+++ helpers/readers.py
@@ -6,6 +6,7 @@ import numpy as np
 import pandas as pd
 import os
 
+
 class Reader():
     def __init__(self, folder_path):
         """
@@ -14,16 +15,16 @@ class Reader():
         """
         self.path = folder_path
 
-    def get_folders(self, type : str = 'all') -> str:
+    def get_folders(self, which: str = 'all') -> str:
         """
         Gets folders containing data based on the desired type (2D, 3D, all)
-        :param type: string mentioning the type of data that we want to read from the folder, either '2D' or '3D' or 'all'
+        :param which: string mentioning the type of data that we want to read from the folder, either '2D' or '3D' or 'all'
         :return: returns a list with folders
         """
-        if type == 'all':
-            self.folders = [os.listdir(self.path)]
+        if which == 'all':
+            self.folders = [file for file in os.listdir(self.path)]
         else:
-            self.folders = [file for file in os.listdir(self.path) if type in file]
+            self.folders = [file for file in os.listdir(self.path) if which in file]
         return self.folders
 
     def set_folder(self, folder):
@@ -34,7 +35,7 @@ class Reader():
         """
         self.folder = folder
 
-    def get_files_from_folder(self, folder_number : int = None, path : str = None) -> list:
+    def get_files_from_folder(self, folder_number: int = None, path: str = None) -> list:
         """
         Gets the files inside the folder, either directly from a path or from a folder number from the get_folder method.
         :param folder_number: int mentioning which folder from the folder list (from get_folder method) if path is None
@@ -47,7 +48,7 @@ class Reader():
                 raise ValueError(f"Please enter a path or a folder number")
             self.files = [file for file in os.listdir(self.path)]
         else:
-            self.files = os.listdir(os.path.join(self.path,self.folders[folder_number]))
+            self.files = os.listdir(os.path.join(self.path, self.folders[folder_number]))
         self.set_folder(os.path.join(self.path, self.folders[folder_number]))
         return self.files
 
@@ -59,42 +60,57 @@ class Reader():
         """
         self.file = file
 
-    def filter(self, type : str) -> list:
+    def filter(self, which: str) -> list:
         """
         Filters the files in the folder based on the desired type (.png, .csv, .txt, ...)
-        :param type: extension of the desired file type
+        :param which: extension of the desired file type
         :return: filtered list of files
         """
-        if type is None:
+        if which is None:
             return self.files
         else:
-            self.files = [file for file in self.files if file.endswith(type)]
+            self.files = [file for file in self.files if file.endswith(which)]
 
     def read_csv(self) -> pd.DataFrame:
         """
-        Reads a .csv file containing raw localization data and renames columns to unified version
+        Reads a .csv file containing raw localization data
+        #TODO: rename columns to unified version
         :return: returns pandas DataFrame
         """
         data = pd.read_csv(os.path.join(self.folder, self.file))
         self.data = data
         return data
 
-    def read_txt(self) -> pd.DataFrame:
+    def read_txt(self, columns: int) -> pd.DataFrame:
         """
-        Reads a .txt file containing raw localization data. Converts it to a DataFrame and renames columns to unified version
+        Reads a .txt file containing raw localization data. Converts it to a DataFrame
+        :param columns: the number of columns to split the text file into
+        #TODO: rename columns to unified version
         :return: returns pandas DataFrame
         """
-        data = pd.read_csv(os.path.join(self.path, self.file, skiprows=[0]))
-        data = data[data.columns[0].str.split(' ', 4, expand=True)]
+        data = pd.read_csv(os.path.join(self.folder, self.file), skiprows=[0])
+        data = data[data.columns[0]].str.split(' ', columns, expand=True)
         self.data = data
         return data
 
-    def get_xyz_array(self) -> np.array:
+    def extract_xyz(self, column_names: list) -> pd.DataFrame:
         """
-        Converts the df to a numpy array containing only the x,y,z coordinates.
-        :param df: DataFrame to be converted to array
-        :return: returns the numpy array
+        Extracts a data frame from the working data frame containing only the x,y,z coordinates.
+        :param column_names: list which explains the names of the original columns
+        :return: data frame
         """
-        arr = np.array(self.data[['x','y','z']])
-        self.array = arr
-        return arr
+        df = pd.DataFrame()
+        df[['x', 'y', 'z']] = self.data[column_names].astype(float)
+        self.df_xyz = df
+        return self.df_xyz
+
+    def extract_xy(self, column_indices: list) -> pd.DataFrame:
+        """
+        Extract a data frame from the working data frame containing only the x and y coordinates.
+        :param column_indices: list containing int that are the column indices containing x and y coordinates in original data frame.
+        :return: data frame
+        """
+        df = pd.DataFrame()
+        df[['x', 'y']] = self.data[column_indices].astype(float)
+        self.df_xy = df
+        return self.df_xy
---
+++ helpers/visualization.py
@@ -0,0 +1,70 @@
+"""
+Class for visualizing point cloud data
+"""
+
+import plotly.express as px
+import pandas as pd
+import numpy as np
+from skimage import exposure
+
+class Visualization():
+    def __init__(self, df : pd.DataFrame):
+        self.df = df
+        self.scatter = []
+    def get_3d_scatter(self):
+        """
+        Gets the interactive 3D scatter plot which can be visualized with the show() method
+        :return: plotly scatter plot
+        """
+        scatter = px.scatter_3d(self.df,
+                                x = self.df['x'],
+                                y = self.df['y'],
+                                z = self.df['z'],
+                                opacity = 0.5)
+        #                        template = "plotly_dark")
+        fig_traces = []
+        for trace in range(len(scatter["data"])):
+            fig_traces.append(scatter["data"][trace])
+
+        for traces in fig_traces:
+            scatter.append_trace(traces, row = 1, col = 1)
+
+        scatter.update_traces(marker = dict(size = 0.5,
+                                            color = 'cyan'),
+                              selector = dict(mode = 'markers'))
+        scatter.update_layout(height = 400, width = 700, template='plotly_dark')
+        scatter.update_xaxes(showgrid=False)
+        scatter.update_yaxes(showgrid=False)
+        self.scatter = scatter
+        return scatter
+
+    def get_2d_scatter(self):
+        """
+        Gets the interactive 3D scatter plot which can be visualized with the show() method
+        :return: plotly scatter plot
+        """
+        if 'z' in self.df.columns:
+            raise ValueError(f"Please use a 2D dataset")
+        else:
+            scatter = px.scatter(self.df,
+                                 x = self.df['x'],
+                                 y = self.df['y'],
+                                 opacity = 0.5)
+            self.scatter = scatter
+        return scatter
+
+    def loc2img(self, pixel_size : int = 10):
+        """
+        Method taken from P. Kollmannsberger for reconstructing 2D localizations into an image
+        :param x:
+        :param y:
+        :param pixel_size: size of the pixel for visualization
+        :return: image
+        """
+        x = self.df['x']
+        y = self.df['y']
+        xbins = np.arange(x.min(), x.max()+1, pixel_size)
+        ybins = np.arange(y.min(), y.max()+1, pixel_size)
+        img, xe, ye = np.histogram2d(y, x, bins=(ybins, xbins))
+        equalized = exposure.equalize_hist(img)
+        return img, equalized
