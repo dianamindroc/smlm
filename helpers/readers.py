d53fc8489af507c68da7741aa3f1808136c8b472
Diana <dianamindrocf@gmail.com>
2022-03-16 18:04:32 +0100

    add functionality in reader

--- helpers/readers.py
+++ helpers/readers.py
@@ -26,38 +26,45 @@ class Reader():
             self.folders = [file for file in os.listdir(self.path) if which in file]
         return self.folders
 
-    def set_folder(self, folder):
+    def set_folder(self, folder: int = None):
         """
         Sets working folder
-        :param folder:
+        :param folder: number of folder inside the main folder; if None, then the main folder is considered the working folder.
         :return: None
         """
-        self.folder = folder
+        if folder is None:
+            self.folder = self.path
+        else:
+            self.folder = os.path.join(self.path, self.folders[folder])
 
     def get_files_from_folder(self, folder_number: int = None, path: str = None) -> list:
         """
         Gets the files inside the folder, either directly from a path or from a folder number from the get_folder method.
         :param folder_number: int mentioning which folder from the folder list (from get_folder method) if path is None
-        :param path: path to folder to read files from if folder_number is None
+        :param path: absolute path to folder to read files from if folder_number is None
         :param type: string to mention the extension of files to get from folder, e.g. images .png or localizations .csv
         :return: returns a list with the files
         """
         if folder_number is None:
             if path is None:
-                raise ValueError(f"Please enter a path or a folder number")
-            self.files = [file for file in os.listdir(self.path)]
+                if self.folder is None:
+                    raise ValueError(f"Please enter a path or a folder number")
+                else:
+                    self.files = os.listdir(self.folder)
+            else:
+                self.files = os.listdir(path)
         else:
             self.files = os.listdir(os.path.join(self.path, self.folders[folder_number]))
-        self.set_folder(os.path.join(self.path, self.folders[folder_number]))
+            self.set_folder(os.path.join(self.path, self.folders[folder_number]))
         return self.files
 
-    def set_file(self, file):
+    def set_file(self, file: int):
         """
         Set the file to be used for further operations
-        :param file: path to the file
+        :param file: int to mention which file from files list to set as working file.
         :return: None
         """
-        self.file = file
+        self.file = self.files[file]
 
     def filter(self, which: str) -> list:
         """
@@ -75,7 +82,7 @@ class Reader():
         """
         Reads a .csv file containing raw localization data
         #TODO: rename columns to unified version
-        :return: returns pandas DataFrame
+        :return: pandas DataFrame
         """
         data = pd.read_csv(os.path.join(self.folder, self.file))
         self.data = data
@@ -86,31 +93,35 @@ class Reader():
         Reads a .txt file containing raw localization data. Converts it to a DataFrame
         :param columns: the number of columns to split the text file into
         #TODO: rename columns to unified version
-        :return: returns pandas DataFrame
+        :return: pandas DataFrame
         """
         data = pd.read_csv(os.path.join(self.folder, self.file), skiprows=[0])
         data = data[data.columns[0]].str.split(' ', columns, expand=True)
         self.data = data
         return data
 
-    def extract_xyz(self, column_names: list) -> pd.DataFrame:
+    def extract_xyz(self, column_names: list, save: bool = False) -> pd.DataFrame:
         """
         Extracts a data frame from the working data frame containing only the x,y,z coordinates.
-        :param column_names: list which explains the names of the original columns
-        :return: data frame
+        :param column_names: list which explains the names of the original columns which contain x,y,z coordinates.
+        :return: pandas DataFrame
         """
         df = pd.DataFrame()
         df[['x', 'y', 'z']] = self.data[column_names].astype(float)
         self.df_xyz = df
+        if save:
+            self.df_xyz.to_csv(os.path.join(self.path, self.folder, self.file + '_xyz.csv'), columns=['x','y','z'], sep=';', index=False)
         return self.df_xyz
 
-    def extract_xy(self, column_indices: list) -> pd.DataFrame:
+    def extract_xy(self, column_indices: list, save: bool = False) -> pd.DataFrame:
         """
         Extract a data frame from the working data frame containing only the x and y coordinates.
         :param column_indices: list containing int that are the column indices containing x and y coordinates in original data frame.
-        :return: data frame
+        :return: pandas DataFrame
         """
         df = pd.DataFrame()
         df[['x', 'y']] = self.data[column_indices].astype(float)
         self.df_xy = df
+        if save:
+            self.df_xy.to_csv(os.path.join(self.path, self.folder, self.file + '_xyz.csv'), columns=['x','y'], sep=';', index=False)
         return self.df_xy
